989c8cdc14f5e23101eace2f00016d31183b1616
jingyang <3161362058@qq.com>
2024-07-31 11:21:06 +0800

    fix BaiDuAnalytics

--- projects/fastgpt/app/BaiDuAnalytics.tsx
+++ projects/fastgpt/app/BaiDuAnalytics.tsx
@@ -3,26 +3,16 @@
 import Script from 'next/script';
 
 const BaiDuAnalytics = () => {
+  const key = process.env.NEXT_PUBLIC_BAIDU_TONGJI;
+
   return (
     <>
-      {process.env.NEXT_PUBLIC_BAIDU_TONGJI ? (
-        <>
-          <Script
-            id="baidu-tongji"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-              var _hmt = _hmt || [];
-              (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?${process.env.NEXT_PUBLIC_BAIDU_TONGJI}";
-                var s = document.getElementsByTagName("script")[0]; 
-                s.parentNode.insertBefore(hm, s);
-              })();
-            `
-            }}
-          />
-        </>
+      {key ? (
+        <Script
+          id="baidu-tongji"
+          strategy="afterInteractive"
+          src={`https://hm.baidu.com/hm.js?${key}`}
+        />
       ) : (
         <></>
       )}

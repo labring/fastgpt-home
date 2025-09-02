a1fb3b5acc10c78b97b30e25156c1224f6ec0102
Archer <545436317@qq.com>
2025-09-02 16:35:11 +0800

    remove google (#106)

--- projects/fastgpt/app/layout.tsx
+++ projects/fastgpt/app/layout.tsx
@@ -1,7 +1,6 @@
 import BaiDuAnalytics from '@/app/BaiDuAnalytics';
 import ClarityAnalytics from '@/app/ClarityAnalytics';
 import RybbitAnalytics from '@/app/RybbitAnalytics';
-import GoogleAnalytics from '@/app/GoogleAnalytics';
 // import { TailwindIndicator } from '@/components/TailwindIndicator';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { siteConfig } from '@/config/site';
@@ -13,7 +12,6 @@ import '@/styles/plyr.css';
 import { Analytics } from '@vercel/analytics/react';
 import { Viewport } from 'next';
 import { Inter as FontSans } from 'next/font/google';
-import Script from 'next/script';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -51,39 +49,13 @@ export default async function RootLayout({
 
   return (
     <html lang={(lang && lang[0]) || defaultLocale} suppressHydrationWarning>
-      <head>
-        {!isChineseDomain && (
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-W9HPZZ22');`
-            }}
-          />
-        )}
-      </head>
       <body className={cn('min-h-screen font-sans antialiased', fontSans.variable)}>
-        {!isChineseDomain && (
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-W9HPZZ22"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-        )}
         <ThemeProvider attribute="class" defaultTheme={siteConfig.nextThemeColor} enableSystem={false} forcedTheme="dark">
           {children}
           {/* <Footer /> */}
           <Analytics />
           {/* <TailwindIndicator /> */}
         </ThemeProvider>
-        <GoogleAnalytics />
         <BaiDuAnalytics />
         <ClarityAnalytics />
         <RybbitAnalytics />
